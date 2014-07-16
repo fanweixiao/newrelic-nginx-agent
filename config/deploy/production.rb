2185743a3fd70ec85955198ab899a7d16720b9b3
Phill Sparks <phill@crowdlab.com>
2014-07-16 13:46:12 +0100

    Add tail03 to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server "tail01.allchannelsopen.com", roles: %w{web app}
 server "tail02.allchannelsopen.com", roles: %w{web app}
+server "tail03.allchannelsopen.com", roles: %w{web app}
 server "head01.allchannelsopen.com", roles: %w{web app}
